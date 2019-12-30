000b18cefb734070b809dd75363c1302133e721a
Julian Gruber <julian@juliangruber.com>
2019-12-30 19:04:35 +0100

    add naive batching

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const { spawn } = require('child_process')
 const { join } = require('path')
 const githubUrlToObject = require('github-url-to-object')
 const Spinnies = require('spinnies')
+const itBatch = require('it-batch')
 
 const run = async (dir, cmd) => {
   const segs = cmd.split(' ')
@@ -18,6 +19,9 @@ const run = async (dir, cmd) => {
 }
 
 const download = async (pkg, dir) => {
+  if (!pkg.repository) {
+    throw new Error('No repository set')
+  }
   const obj = githubUrlToObject(pkg.repository.url)
   let res = await fetch(
     `https://github.com/${obj.user}/${obj.repo}/archive/${pkg.version}.tar.gz`
@@ -28,7 +32,7 @@ const download = async (pkg, dir) => {
     )
   }
   if (!res.ok) {
-    throw new Error('Unable to download source code')
+    throw new Error('No matching GitHub release found')
   }
   await fs.writeFile(`${dir}/tgz`, await res.buffer())
   await run(dir, 'tar -xzf tgz --strip=1')
@@ -44,39 +48,47 @@ const main = async () => {
   }
   const spinnies = new Spinnies()
 
-  for await (const dependant of dependants(root.name)) {
-    spinnies.add(dependant, {
-      text: `[${dependant}] Loading package information`
-    })
-    const res = await fetch(`https://registry.npmjs.org/${dependant}`)
-    const body = await res.json()
-    const pkg = body.versions[body['dist-tags'].latest]
-    const range = { ...pkg.devDependencies, ...pkg.dependencies }[root.name]
-    if (range && semver.satisfies(root.version, range)) {
-      const dir = join(
-        tmpdir(),
-        [pkg.name, pkg.version, Date.now(), Math.random()].join('-')
-      )
-      spinnies.update(pkg.name, { text: `[${dependant}] Downloading package` })
-      await fs.mkdir(dir)
-      try {
-        await download(pkg, dir)
-      } catch (err) {
-        console.error(err.message)
-        continue
-      }
-      spinnies.update(pkg.name, {
-        text: `[${dependant}] Installing dependencies`
+  for await (const batch of itBatch(dependants(root.name), 5)) {
+    await Promise.all(
+      batch.map(async dependant => {
+        spinnies.add(dependant, {
+          text: `[${dependant}] Loading package information`
+        })
+        const res = await fetch(`https://registry.npmjs.org/${dependant}`)
+        const body = await res.json()
+        const pkg = body.versions[body['dist-tags'].latest]
+        const range = { ...pkg.devDependencies, ...pkg.dependencies }[root.name]
+        if (range && semver.satisfies(root.version, range)) {
+          const dir = join(
+            tmpdir(),
+            [pkg.name, pkg.version, Date.now(), Math.random()].join('-')
+          )
+          spinnies.update(pkg.name, {
+            text: `[${dependant}] Downloading package`
+          })
+          await fs.mkdir(dir)
+          try {
+            await download(pkg, dir)
+          } catch (err) {
+            spinnies.fail(pkg.name, { text: `[${pkg.name}] ${err.message}` })
+            return
+          }
+          spinnies.update(pkg.name, {
+            text: `[${dependant}] Installing dependencies`
+          })
+          await run(dir, 'npm install')
+          spinnies.update(pkg.name, {
+            text: `[${dependant}] Running test suite`
+          })
+          await run(dir, 'npm test')
+          spinnies.succeed(pkg.name)
+        } else {
+          spinnies.fail(pkg.name, {
+            text: `[${pkg.name}] Package not found in dependant's latest version`
+          })
+        }
       })
-      await run(dir, 'npm install')
-      spinnies.update(pkg.name, { text: `[${dependant}] Running test suite` })
-      await run(dir, 'npm test')
-      spinnies.succeed(pkg.name)
-    } else {
-      spinnies.fail(pkg.name, {
-        text: `[${dependant}] Package not found in dependant's latest version`
-      })
-    }
+    )
   }
 }
 
