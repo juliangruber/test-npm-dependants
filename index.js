c10f1e964a078ef1babc4d9ef361db3bfe2ae144
Julian Gruber <julian@juliangruber.com>
2020-01-01 18:32:48 +0100

    refactor using fetch-package-source

--- index.js
+++ index.js
@@ -8,33 +8,13 @@ const { promises: fs } = require('fs')
 const { promisify } = require('util')
 const { spawn } = require('child_process')
 const { join } = require('path')
-const githubUrlToObject = require('github-url-to-object')
 const Spinnies = require('spinnies')
+const fetchPackageSource = require('fetch-package-source')
 
 const run = async (dir, cmd) => {
   const segs = cmd.split(' ')
   const ps = spawn(segs[0], segs.slice(1), { cwd: dir })
   await promisify(ps.once.bind(ps))('exit')
-}
-
-const download = async (pkg, dir) => {
-  if (!pkg.repository) {
-    throw new Error('No repository set')
-  }
-  const obj = githubUrlToObject(pkg.repository.url)
-  let res = await fetch(
-    `https://github.com/${obj.user}/${obj.repo}/archive/${pkg.version}.tar.gz`
-  )
-  if (!res.ok) {
-    res = await fetch(
-      `https://github.com/${obj.user}/${obj.repo}/archive/v${pkg.version}.tar.gz`
-    )
-  }
-  if (!res.ok) {
-    throw new Error('No matching GitHub release found')
-  }
-  await fs.writeFile(`${dir}/tgz`, await res.buffer())
-  await run(dir, 'tar -xzf tgz --strip=1')
 }
 
 const main = async () => {
@@ -59,43 +39,50 @@ const main = async () => {
           const res = await fetch(`https://registry.npmjs.org/${dependant}`)
           const body = await res.json()
           const pkg = body.versions[body['dist-tags'].latest]
-          const range = { ...pkg.devDependencies, ...pkg.dependencies }[
-            root.name
-          ]
-          if (range && semver.satisfies(root.version, range)) {
-            const dir = join(
-              tmpdir(),
-              [
-                pkg.name.replace('/', '-'),
-                pkg.version,
-                Date.now(),
-                Math.random()
-              ].join('-')
-            )
-            spinnies.update(pkg.name, {
-              text: `[${dependant}] Downloading package`
-            })
-            await fs.mkdir(dir)
-            try {
-              await download(pkg, dir)
-            } catch (err) {
-              spinnies.fail(pkg.name, { text: `[${pkg.name}] ${err.message}` })
-              continue
-            }
-            spinnies.update(pkg.name, {
-              text: `[${dependant}] Installing dependencies`
-            })
-            await run(dir, 'npm install')
-            spinnies.update(pkg.name, {
-              text: `[${dependant}] Running test suite`
-            })
-            await run(dir, 'npm test')
-            spinnies.succeed(pkg.name)
-          } else {
+          if (!pkg.repository) {
+            spinnies.fail(pkg.name, { text: `[${pkg.name}] No repository set` })
+            continue
+          }
+          const allDependencies = {
+            ...pkg.devDependencies,
+            ...pkg.dependencies
+          }
+          const range = allDependencies[root.name]
+          if (!range || !semver.satisfies(root.version, range)) {
             spinnies.fail(pkg.name, {
               text: `[${pkg.name}] Package not found in dependant's latest version`
             })
+            continue
           }
+
+          const dir = join(
+            tmpdir(),
+            [
+              pkg.name.replace('/', '-'),
+              pkg.version,
+              Date.now(),
+              Math.random()
+            ].join('-')
+          )
+          spinnies.update(pkg.name, {
+            text: `[${dependant}] Downloading package`
+          })
+          await fs.mkdir(dir)
+          try {
+            await fetchPackageSource(pkg.repository.url, pkg.version, dir)
+          } catch (err) {
+            spinnies.fail(pkg.name, { text: `[${pkg.name}] ${err.message}` })
+            continue
+          }
+          spinnies.update(pkg.name, {
+            text: `[${dependant}] Installing dependencies`
+          })
+          await run(dir, 'npm install')
+          spinnies.update(pkg.name, {
+            text: `[${dependant}] Running test suite`
+          })
+          await run(dir, 'npm test')
+          spinnies.succeed(pkg.name)
         }
       })
   )
